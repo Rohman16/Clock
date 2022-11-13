5aafc3eb16a520e3676013ea0acc15b779168a7d
rohman <rohman.thelearner@gmail.com>
2022-11-14 05:00:12 +0700

    Continue Set exact Hours Hand

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const btn = document.getElementById("rotate");
 const btnLeft = document.getElementById("rotate-left");
+const myHour = document.querySelector(".min hand");
 let k = 0;
 let m = 0;
 
 btn.addEventListener("click", () => {
   m += 5;
   const test = document.getElementById("tester");
-  const obj = document.querySelector(".hand");
-  obj.style.transform = `translateY(-50%) rotate(${m}deg)`;
+  const obj = document.querySelector(".min");
+  obj.style.transform = `translateX(-50%) rotate(${m}deg)`;
   test.innerHTML = k;
   k += 3;
 });
@@ -16,17 +17,28 @@ btnLeft.addEventListener("click", () => {
   m -= 5;
   const test = document.getElementById("tester");
   const obj = document.querySelector(".hand");
-  obj.style.transform = `translateY(-50%) rotate(${m}deg)`;
+  obj.style.transform = `translateX(-50%) rotate(${m}deg)`;
   test.innerHTML = k;
   k -= 3;
 });
 
 function rtate() {
-  m += 5;
+  const time = new Date();
+  const numSecond = time.getSeconds();
+  const numMinute = time.getMinutes();
+  const numHour = time.getHours();
+  const secondDeg = (numSecond / 60) * 360 + 180;
+  const minuteDeg = (numMinute / 60) * 360 + 180;
+  const hourDeg = (numHour / 12) * 360 + 180;
+  // m += 180;
   const test = document.getElementById("tester");
-  const obj = document.querySelector(".hand");
-  obj.style.transform = `translateY(-50%) rotate(${m}deg)`;
-  test.innerHTML = k;
+  const second = document.querySelector(".second");
+  const minute = document.querySelector(".min");
+  const hour = document.querySelector(".hour");
+  second.style.transform = `translateX(-50%) rotate(${secondDeg}deg)`;
+  minute.style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
+  hour.style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
+  test.innerHTML = `${numSecond}:${numMinute}`;
   k += 3;
 }
 
