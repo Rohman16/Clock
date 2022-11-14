9d0a6b593d199ce18cbc4eae9d54b3f31a8a236b
rohman <rohman.thelearner@gmail.com>
2022-11-15 06:39:21 +0700

    Contine Design

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 function rtate() {
-    const time = new Date();
-    const numSecond = time.getSeconds();
-    let numSecondUse = "";
-    if (numSecond < 10) {
-        numSecondUse = "0" + numSecond
-    } else {
-        numSecondUse = numSecond;
-    }
-    const numMinute = time.getMinutes();
-    let numMinuteUse = "";
-    if (numMinute < 10) {
-        numMinuteUse = "0" + numMinute
-    } else {
-        numMinuteUse = numMinute;
-    }
-    const numHour = time.getHours();
-    const secondDeg = (numSecond / 60) * 360 + 180;
-    const minuteDeg = (numMinute / 60) * 360 + 180;
-    const hourDeg = ((numHour + (numMinute / 60)) / 12) * 360 + 180;
+  const time = new Date();
+  const numSecond = time.getSeconds();
+  let numSecondUse = "";
+  if (numSecond < 10) {
+    numSecondUse = "0" + numSecond;
+  } else {
+    numSecondUse = numSecond;
+  }
+  const numMinute = time.getMinutes();
+  let numMinuteUse = "";
+  if (numMinute < 10) {
+    numMinuteUse = "0" + numMinute;
+  } else {
+    numMinuteUse = numMinute;
+  }
+  const numHour = time.getHours();
+  const secondDeg = (numSecond / 60) * 360 + 180;
+  const minuteDeg = (numMinute / 60) * 360 + 180;
+  const hourDeg = ((numHour + numMinute / 60) / 12) * 360 + 180;
 
-    const digital = document.getElementById("digital");
-    const second = document.querySelector(".second");
-    const minute = document.querySelector(".min");
-    const hour = document.querySelector(".hour");
-    second.style.transform = `translateX(-50%) rotate(${secondDeg}deg)`;
-    minute.style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
-    hour.style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
-    digital.innerHTML = `${numHour}:${numMinute}:${numSecondUse}`;
-    k += 3;
+  const digital = document.getElementById("digital");
+  const second = document.querySelector(".second");
+  const minute = document.querySelector(".min");
+  const hour = document.querySelector(".hour");
+  second.style.transform = `translateX(-50%) rotate(${secondDeg}deg)`;
+  minute.style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
+  hour.style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
+  digital.innerHTML = `${numHour}:${numMinute}:${numSecondUse}`;
 }
 
 setInterval(rtate, 1000);
